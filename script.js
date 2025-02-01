3025bd9cf8a1c0c87324dc092ff09bed3dee6054
Saha1999 <aleksandraluchcko05@mail.com>
2025-02-02 00:10:52 +0300

    дз

---
+++ script.js
@@ -0,0 +1,41 @@
+//задание 1
+let a;
+alert(a=10);
+alert(a=20);
+//задание 2
+let iphone;
+alert(iphone=2007);
+//задание 3
+let js = "Brendan Eich"
+alert(js);
+//Задание 4
+let z = 10
+let x = 2
+alert (z + x);
+//Задание 5
+alert(x ** 5);
+//задание 6
+let h = 9
+let b = 2
+alert(h % b);
+//Задание 7
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num -=3;
+num ++;
+num --;
+alert(num);
+//Задание 8
+let age = prompt('Сколько вам лет?)')
+alert(age);
+//Задание 9
+let user = {
+    name: "Александра",
+    age: 20,
+    isAdmin: true
+};
+  //Задание10
+  const name = prompt('Введите ваше имя:');
+  alert(`Привет, ${name}!`)
